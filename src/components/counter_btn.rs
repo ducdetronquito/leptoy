6c649552cfbbe6e943c7e78fd53ffb05fa6e08e6
ducdetronquito <g.paulet@smartway.ai>
2025-12-28 17:48:30 +0100

    feat: Define button counter state as an automerge doc

--- src/components/counter_btn.rs
+++ src/components/counter_btn.rs
@@ -1,15 +1,26 @@
+use automerge::{transaction::Transactable, AutoCommit, ReadDoc, ScalarValue};
 use leptos::prelude::*;
 
-/// A parameterized incrementing button
+fn get_counter_value(read_signal: &ReadSignal<AutoCommit>) -> u64 {
+    read_signal
+        .get()
+        .get(automerge::ROOT, "counter")
+        .expect("Get counter value")
+        .map(|(value, _)| value.to_u64().unwrap())
+        .unwrap()
+}
 #[component]
-pub fn Button(#[prop(default = 1)] increment: i32) -> impl IntoView {
-    let (count, set_count) = signal(0);
+pub fn Button(#[prop(default = 1)] increment: i64) -> impl IntoView {
+    let mut doc = AutoCommit::new();
+    doc.put(automerge::ROOT, "counter", ScalarValue::counter(0))
+        .expect("Failed to define counter");
+    let (doc_read, doc_write) = signal(doc);
     view! {
         <button on:click=move |_| {
-            set_count.set(count.get() + increment)
-        }>
-
-            "Click me: " {count}
-        </button>
+            doc_write
+                .write()
+                .increment(&automerge::ROOT, "counter", increment)
+                .expect("Failed to increment counter")
+        }>"Click me: " {move || { get_counter_value(&doc_read) }}</button>
     }
 }
